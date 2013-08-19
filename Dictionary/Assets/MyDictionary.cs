400968d52590343cd29e52752ef0edc55f3715c5
alexokita <alex.okita@gmail.com>
2013-08-18 17:30:29 -0700

    more added dictionary and events
    lots more to come

---
+++ Dictionary/Assets/MyDictionary.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyDictionary : MonoBehaviour {
+	 Dictionary<string,int> dict;
+	// Use this for initialization
+	void Start () {
+		dict = new Dictionary<string,int>();
+		dict.Add("Zombies",10);
+		Debug.Log ( dict["Zombies"] );
+				
+		Dictionary<int, Object> obs = new Dictionary<int, Object>();
+		Object[] allObjects = GameObject.FindObjectsOfType( typeof( Object ) ) as Object[];
+		
+		for( int i = 0; i < allObjects.Length; i++ ) {
+			obs.Add( i, allObjects[i] );
+		}
+		Debug.Log( obs[1] );
+		Debug.Log( obs[2] );
+		Debug.Log( obs[3] );
+	}
+}
