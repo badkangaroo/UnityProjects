3830c5976796a3b8b02b4470194f6ab58e3b1270
alex okita <alex.okita@gmail.com>
2014-10-19 16:19:50 -0700

    adding LogicScene

---
+++ LogicScene/Assets/Logic.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour
+{
+    public GameObject A_Cube;
+    public GameObject B_Cube;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Color col = Color.red;
+
+        float Ax = A_Cube.transform.position.x;
+        float Ay = A_Cube.transform.position.y;
+        float Bx = B_Cube.transform.position.x;
+        float By = B_Cube.transform.position.y;
+
+        float d = Ax + Bx;
+
+        if (Ax > Bx)
+        {
+            col = Color.black;
+            if (d > 10.0f)
+            {
+                col = Color.cyan;
+                float e = Ay + By;
+                if (e > 1.0f)
+                {
+                    col = Color.green;
+                }
+            }
+        } else if (Ax <= Bx)
+        {
+            col = Color.blue;
+        }
+
+        renderer.material.color = col;
+    }
+}
