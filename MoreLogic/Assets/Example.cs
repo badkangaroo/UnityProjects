65cd2c1b94754e7315dac50ce344d152eb57e5ec
alex okita <alex.okita@gmail.com>
2014-10-19 16:19:04 -0700

    more things

--- MoreLogic/Assets/Example.cs
+++ MoreLogic/Assets/Example.cs
@@ -1,24 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Example : MonoBehaviour {
-	public PrimitiveType primitiveType;
-	GameObject obj;
-	public enum colorType{red,blue,green}
-	public colorType myColor;
+public class Example : MonoBehaviour
+{
+    public PrimitiveType primitiveType;
+    GameObject obj;
+    public enum colorType
+    {
+        red,
+        blue,
+        green
+    }
+    public colorType myColor;
 
-	// Use this for initialization
-	void Start () {
-		obj = GameObject.CreatePrimitive( primitiveType );
-		if ( myColor == colorType.red ) {
-			obj.renderer.material.color = Color.red;
-		}
-		if ( myColor == colorType.blue ) {
-			obj.renderer.material.color = Color.blue;
-		}
-		if ( myColor == colorType.green ) {
-			obj.renderer.material.color = Color.green;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        obj = GameObject.CreatePrimitive(primitiveType);
+        if (myColor == colorType.red)
+        {
+            obj.renderer.material.color = Color.red;
+        }
+        if (myColor == colorType.blue)
+        {
+            obj.renderer.material.color = Color.blue;
+        }
+        if (myColor == colorType.green)
+        {
+            obj.renderer.material.color = Color.green;
+        }
+    }
 
+    void Update()
+    {
+        renderer.material.color = Color.red;
+    }
 }
