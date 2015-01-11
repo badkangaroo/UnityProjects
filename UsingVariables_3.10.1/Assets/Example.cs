5ba8712e1fa5d4c1485684dfda41d634d19ac9c8
alex okita <alex@occipital.com>
2015-01-11 15:09:24 -0800

    VR Updating section 3's example files.

---
+++ UsingVariables_3.10.1/Assets/Example.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+    public int SomeInt;
+    public int OtherInt = 7;
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
---
+++ UsingVariables_3.10.2/Assets/Example.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+//		int MyVariable = 7;
+//		print (MyVariable);
+//		MyVariable = 13
+//		print (MyVariable);
+//		MyVariable = 3;
+//		print (MyVariable);
+//		MyVariable = 73;
+//		print (MyVariable);
+
+
+//		int MyVariable = 7;
+//		print (MyVariable + MyVariable);//prints 14
+//		print (7 + 7);//prints 14
+
+//		int MyVariable = 7;
+//		int MyOtherVariable = 3;
+//		print (MyVariable * MyOtherVariable);//prints 21
+//		print (MyVariable * 3);//prints 21
+
+		int MyVariable = 7;
+		int MyOtherVariable = 3;
+		print (MyVariable * MyOtherVariable);
+		MyVariable = MyOtherVariable; //what's happening here?
+		print (MyVariable * MyOtherVariable);
+	}
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+}
