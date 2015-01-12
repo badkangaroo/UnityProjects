5f9a85aa7c799938578d2ceff61089422d667094
alex okita <alex.okita@gmail.com>
2015-01-11 16:12:12 -0800

    VR: Updated the 3.10.3 project.

---
+++ UsingVariables_3.10.3/Assets/SphereController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+	public float Control;
+	public float OtherControl;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = new Vector3 (Control, Control + Control, Control * Control);
+
+		//transform.position = new Vector3 (
+		//Mathf.Sin (Control) * OtherControl,
+		//Mathf.Cos (Control) * OtherControl,
+		//Control * OtherControl);
+		float x = Mathf.Sin (Control) * OtherControl;
+		float y = Mathf.Cos (Control) * OtherControl;
+		float z = Control * OtherControl;
+		transform.position = new Vector3 (x, y, z);
+	}
+}
