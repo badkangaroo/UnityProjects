4d71d315cabe609b15b6fae56f920990f4be7110
alex okita <alex.okita@gmail.com>
2015-04-20 02:18:14 -0700

    more fixups.

--- Enums/Assets/MonsterSpawner.cs
+++ Enums/Assets/MonsterSpawner.cs
@@ -18,15 +18,15 @@ public class MonsterSpawner : MonoBehaviour
 		obj = GameObject.CreatePrimitive(primitiveType);
 		if (myColor == colorType.red)
 		{
-			obj.renderer.material.color = Color.red;
+			obj.GetComponent<Renderer>().material.color = Color.red;
 		}
 		if (myColor == colorType.blue)
 		{
-			obj.renderer.material.color = Color.blue;
+			obj.GetComponent<Renderer>().material.color = Color.blue;
 		}
 		if (myColor == colorType.green)
 		{
-			obj.renderer.material.color = Color.green;
+			obj.GetComponent<Renderer>().material.color = Color.green;
 		}
 	}
 
