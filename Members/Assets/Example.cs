ee90082c27b68a8485c117533bf4e5c5905bdf73
alex okita <alex.okita@gmail.com>
2015-07-20 01:05:14 -0700

    UnityProjects adding in a new Project called Members as an example for section 6.3.2.1

---
+++ Members/Assets/Example.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Members
+{
+	public void FirstFunction()
+	{
+		Debug.Log("First Function");
+	}
+}
+
+public class Example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Members m = new Members();
+		m.FirstFunction();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
